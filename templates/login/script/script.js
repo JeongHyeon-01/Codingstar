15a8f2e530c0fdca24da6b12ac87752450adf1d5
sunnyfterrain <sunnyfterrain@gmail.com>
2022-01-30 00:40:44 +0900

    Add: add function & effect  - 실제 인스타그램의 효과 및 기능들을 구현  - id, pw 일정 문자 입력 되어야만 로그인, 가입 버튼 활성화  - pw 입력시 비밀번호 숨기기/표시 버튼 작동

--- templates/login/script/script.js
+++
@@ -1,8 +0,0 @@
-const input = document.querySelectorAll('input');
-const inputBox = document.querySelector('.form-log__idpw');
-
-input.forEach((e) =>
-  e.addEventListener('focus', () => {
-    console.log('yes');
-  })
-);
