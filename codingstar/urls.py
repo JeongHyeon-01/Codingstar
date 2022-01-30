0194a1d9cfa9d4413aabfffedbaf777b4ab40d44
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-01-30 13:48:46 +0900

    linked main_feed page

--- codingstar/urls.py
+++ codingstar/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', include('login.urls')),
+    path('main/', include('main.urls')),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def Main_feed(request):
+    return HttpResponse('Here is main feed page')
