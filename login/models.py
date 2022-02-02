fdf72f5ea58b748da2238537b520bb21cb0d90bf
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-02-02 17:45:25 +0900

    change models

--- login/models.py
+++ login/models.py
@@ -5,8 +5,10 @@ from django.db import models
 # https://velog.io/@seungsang00/Database-Instagram-%EC%8A%A4%ED%82%A4%EB%A7%88-%EB%94%94%EC%9E%90%EC%9D%B8
 # 위의 블로그 ERD의 users부분
 class users(models.Model):
-    id = models.CharField(max_length=100, primary_key=True)
+    nickname = models.CharField(max_length=100, unique = True)
     name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    create_at = models.DateField()
+    email = models.CharField(max_length=100, unique = True, null =True)
+    phone_number = models.CharField(max_length=100, unique = True, null = True)
+    password = models.CharField(max_length=300)
+    create_at = models.DateField(auto_now_add = True)
+    update_at = models.DateField(auto_now = True)
