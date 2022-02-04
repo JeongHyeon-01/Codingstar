e29afa8b8261b1d7b136ab03d0ecab18624deadc
gleehave <gleehave@gmail.com>
2022-02-05 01:43:48 +0900

    view 작성 Main_feed, post_create

--- main/models.py
+++ main/models.py
@@ -1,13 +1,20 @@
 from django.db import models  # django에서 제공하는 라이브러리
 from login import models as user_model # login의 모델에서 작성했던 users 모델을 사용할 겁니다.
 
+class TimeStamedModel(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        abstract = True
+
 # Post 라는 객체는 django의 기본 데이터베이스 모델 기능을 수행할 수 있습니다.
 # author는 작성자를 의미합니다. login의 users 모델을 참조합니다.
 # image는 사진을 의미합니다. 사진이 저장될 수 있도록 ImageField를 사용합니다.
 # caption은 사진에 대한 간략한 설명을 의미합니다.
 # image_likes는 post에서 좋아요를 받는 것을 의미하는데, 일단 만들어만 둡니다. 삭제할 수 있음.
 # post 객체를 조회할때, 작성자(author)와 간략설명(caption)이 찍히게 합니다.
-class Post(models.Model):
+class Post(TimeStamedModel):
+    objects = None
     author = models.ForeignKey(user_model.users, null=True, on_delete=models.CASCADE, related_name='post_author')
     image = models.ImageField(blank=False)
     caption = models.TextField(blank=False)
@@ -21,7 +28,7 @@ class Post(models.Model):
 # 여러개의 포스트(posts)는 여러개의 댓글이 달릴 수 있습니다. Post 모델을 참조합니다.
 # content는 내용을 의미합니다.
 # comment을 조회할 때, 작성자(author)와 내용(content)이 찍히게 합니다.
-class Comment(models.Model):
+class Comment(TimeStamedModel):
     author = models.ForeignKey(user_model.users, null=True, on_delete=models.CASCADE, related_name='comment_author')
     posts = models.ForeignKey(Post, null=True, on_delete=models.CASCADE, related_name='comment_post')
     contents = models.TextField(blank=True)
--- main/views.py
+++ main/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from login import models as user_model # login의 모델에서 작성했던 users 모델을 사용할 겁니다.
+from main import models as post_model
 # Create your views here.
 
 
+# 먼저, 회원 여부를 검증해야 하지만 제외함.
+# 그러므로, 회원 여부에 관계없이 post 전부 출력->1인용 인스타 그램
 def Main_feed(request):
-    return HttpResponse('Here is main feed page')
+    if request.method == "GET":
+        comment_form = CommentForm() # forms.py 에다가 정의 해야함...
+        user = get_object_or_404(user_model, pk=request.users.id)
+        posts = post_model.Post.objects.all().order_by("-create_at")
+
+        serializer = serializers.PostSerializer(posts,many=True)
+
+        return render(request,'main/main.html',{"posts":serializer.data, "comment_form":comment_form})
+
+def post_create(request):
+    if request.method == "GET":
+        form = CreatePostForm()
+        return render(request, 'main/main.html',{"form":form})
+    elif request.method == "POST":
+        user = get_object_or_404(user_model, pk=request.users.id)
+        form = CreatePostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = user
+            post.save()
+        else:
+            print(form.errors)
+        return render(request, 'main/main.html')
+
+
+
